dd1ab6da571040f9da01ab42e06c588d8ec6ba40
M-H-Abdelfadeil <mahmoudhasan509@gmail.com>
2021-02-14 00:21:21 +0200

    create default env

--- app/Helpers/MainHelpers.php
+++ app/Helpers/MainHelpers.php
@@ -2,3 +2,8 @@
 function env($key){
     return $_ENV[$key];
 }
+
+ function setDefaultEnv()
+{
+    $_ENV['DEFAULT_APP_URL']=$_SERVER["REQUEST_SCHEME"]."://".$_SERVER["HTTP_HOST"];
+}
--- index.php
+++ index.php
@@ -3,4 +3,8 @@ include "vendor/autoload.php";
 use Dotenv\Dotenv;
 $dotenv = Dotenv::createImmutable(__DIR__);
 $dotenv->load();
+setDefaultEnv();
 new App\LIB\Core;
+
+
+
